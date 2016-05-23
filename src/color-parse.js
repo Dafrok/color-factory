0611b28beeb13cbe403fb245877e2d7287c9a6d3
Dafrok <o.o@mug.dog>
2016-05-23 19:39:14 +0800

    color-parse

---
+++ src/color-parse.js
@@ -0,0 +1,31 @@
+export default function (color, type) {
+  let RGB = {}
+  let HSL = {}
+  let ALPHA = 1
+  switch (type) {
+    'hex':
+      break
+    'rgb':
+      break
+    'rgba':
+      break
+    'hsl':
+      break
+    'hsla':
+      break
+    'named':
+    'transparent':
+      break
+  }
+  return {
+    toHSL (color) {
+
+    },
+    toRGB (color) {
+
+    },
+    getAlpha (color) {
+
+    }
+  }
+}
--- src/color.js
+++ src/color.js
@@ -1,19 +1,20 @@
 import getColorType from './color-type.js'
+import colorParse from './color-parse.js'
 
 class Color {
   constructor (cssColor) {
     const color = cssColor.replace(/ /g, '')
     const type = this.getColorType(color)
-    switch (type) {
-      'hex':
-      'rgb':
-      'rgba':
-      'hsl':
-      'hsla':
-      'named':
-      'transparent':
-        break
-    }
+    const RGB = colorParse.toRGB(color, type)
+    const HSL = colorParse.toHSL(color, type)
+    const ALPHA = colorParse.getAlpha(color)
+    this.r = RGB[0]
+    this.g = RGB[1]
+    this.b = RGB[2]
+    this.h = HSL[0]
+    this.s = HSL[1]
+    this.l = HSL[2]
+    this.a = ALPHA
   }
   getColorType (color) {
     return getColorType(color)
