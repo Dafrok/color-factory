cc381bb4f099fec7ef67b4c9af01c8a71ac80aab
Dafrok <o.o@mug.dog>
2016-05-23 17:06:41 +0800

    bug fixed

--- src/color-type.js
+++ src/color-type.js
@@ -1,4 +1,5 @@
 import csscolors from 'css-color-names'
+
 function isHex(color) {
   const colorValue = parseInt(color.substring(1, 7), 16)
   return color[0] === '#' && (color.length === 7 || color.length === 4) &&　colorValue >=0 && colorValue <=16777215
@@ -17,7 +18,7 @@ function isNamedColor(color) {
 }
 
 function isLegalRate (rates) {
-  const regexp = /^-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?%$/
+  const regexp = /^-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?$/
   for (let rate of rates) {
     if (rate[rate.length - 1] !== '%' || regexp.test(+rate.slice(0, rate.length - 1))) {
       return false
