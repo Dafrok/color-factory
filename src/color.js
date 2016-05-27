8322bb02ed8a8ad9fe4f1b5f018e51f6f39ff9e7
Dafrok <o.o@mug.dog>
2016-05-27 14:12:26 +0800

    resolve object argument

--- src/color.js
+++ src/color.js
@@ -2,19 +2,30 @@ import getColorType from './color-type.js'
 import colorParse, {parseToHex} from './color-parse.js'
 
 export default class Color {
-  constructor (cssColor) {
-    const color = cssColor.replace(/ /g, '')
-    const type = this.getColorType(color)
-    const RGB = colorParse.toRGB(color, type)
-    const HSL = colorParse.toHSL(color, type)
-    const ALPHA = colorParse.getAlpha(color, type)
-    this.r = RGB.r
-    this.g = RGB.g
-    this.b = RGB.b
-    this.h = HSL.h
-    this.s = HSL.s
-    this.l = HSL.l
-    this.a = ALPHA
+  constructor (cssColor = {}) {
+    if (typeof cssColor === 'string') {
+      const color = cssColor.replace(/ /g, '')
+      const type = this.getColorType(color)
+      const RGB = colorParse.toRGB(color, type)
+      const HSL = colorParse.toHSL(color, type)
+      const ALPHA = colorParse.getAlpha(color, type)
+      this.r = RGB.r
+      this.g = RGB.g
+      this.b = RGB.b
+      this.h = HSL.h
+      this.s = HSL.s
+      this.l = HSL.l
+      this.a = ALPHA
+    } else {
+      this.r = cssColor.r || 0
+      this.g = cssColor.g || 0
+      this.b = cssColor.b || 0
+      this.h = cssColor.h || 0
+      this.s = cssColor.s || 0
+      this.l = cssColor.l || 0
+      this.a = cssColor.a || 0
+    }
+
   }
   getColorType (color) {
     return getColorType(color)
