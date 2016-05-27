7e565bc745ae27796990ddf9a8ca10a5bc25c1e2
Dafrok <o.o@mug.dog>
2016-05-27 11:02:20 +0800

    rollup

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import babel from 'rollup-plugin-babel'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import replace from 'rollup-plugin-replace'
+
+export default {
+    entry: './src/color.js',
+    dest: 'index.js',
+    format: 'umd',
+    moduleName: 'ColorFactory',
+    plugins: [
+        babel({presets: ['es2015-rollup']}),
+        nodeResolve({
+            jsnext: true,
+            main: true,
+            extensions: [ '.js', '.json' ]
+        }),
+        commonjs({
+            include: 'node_modules/**'
+        }),
+        replace({
+            'process.env.NODE_ENV': "'production'"
+        })
+    ]
+}
