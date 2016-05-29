0c80628b6bf8afdbc1d3f3a5489716871db96794
Dafrok <o.o@mug.dog>
2016-05-29 16:27:07 +0800

    optimize

--- src/color-validator.js
+++ src/color-validator.js
@@ -1,5 +1,6 @@
+const regexp = /^-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?$/
+
 export function isLegalRate (rates) {
-  const regexp = /^-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?$/
   for (let rate of rates) {
     if (rate[rate.length - 1] !== '%' || !regexp.test(+rate.slice(0, rate.length - 1))) {
       return false
@@ -9,7 +10,6 @@ export function isLegalRate (rates) {
 }
 
 export function isLegalNumber (numbers) {
-  const regexp = /^-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?$/
   for (let number of numbers) {
     if (!regexp.test(+number)) {
       return false
